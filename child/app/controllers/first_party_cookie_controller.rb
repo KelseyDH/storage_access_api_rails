09d7526e89454ef72ed3a18d27856746dffb63bd
Kelsey Hannan <kelseyh@gmail.com>
2020-01-29 17:05:16 -0800

    add ability to introspect cookies

--- child/app/controllers/first_party_cookie_controller.rb
+++ child/app/controllers/first_party_cookie_controller.rb
@@ -1,5 +1,9 @@
 class FirstPartyCookieController < ApplicationController
 
   def index
+    cookies["serverside"] = {
+      value: 1,
+      expires: 1.years.from_now
+    }
   end
 end
--- parent/app/controllers/home_controller.rb
+++ parent/app/controllers/home_controller.rb
@@ -2,5 +2,15 @@ class HomeController < ApplicationController
   layout "home"
 
   def index
+    cookies["parent_http_only"] = {
+      value: "1",
+      expires: 1.years.from_now,
+      http_only: true
+    }
+
+    cookies["parent"] = {
+      value: "1",
+      expires: 1.years.from_now
+    }
   end
 end
